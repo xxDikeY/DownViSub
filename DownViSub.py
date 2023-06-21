aae8d04a648e85999b8306139177d8f9ddb0700c
xxDikeY <114901775+xxDikeY@users.noreply.github.com>
2023-06-21 15:37:36 +0300

    V 2.1

--- DownViSub.py
+++ DownViSub.py
@@ -83,7 +83,7 @@ def Main():
 
             if len(playlist.videos) == 0:
                 print("Плейлист закрытый или пустой")
-                break
+                continue
 
             all_count = len(playlist.videos)
             count = 1
